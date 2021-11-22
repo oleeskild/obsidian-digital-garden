78227d229abece20c829f63ce8d34b660008d0f6
Ole Eskild Steensen <ole.steensen@knowit.no>
2021-11-23 00:20:17 +0100

    Upload file and trigger webhook

--- main.ts
+++ main.ts
@@ -1,81 +1,46 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { clipboard } from 'electron';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface NotePublisherSettings {
+	uploadFunctionUrl: string;
+	buildWebHook: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: NotePublisherSettings = {
+	uploadFunctionUrl: 'default',
+	buildWebHook: 'default'
+
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class NotePublisher extends Plugin {
+	settings: NotePublisherSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			id: 'publish-note',
+			name: 'Publish Note',
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				let text = editor.getValue();
+				await this.uploadText(view.file.name, text);
+				let fileTitle = view.file.name.replace(".md", "");
+				// new PublishedModal(this.app, `https://ole.dev/notes/${fileTitle}`).open();
+				clipboard.writeText(`https://ole.dev/notes/${fileTitle}`);
+				new Notice(`Successfully published to https://ole.dev/notes/${fileTitle}.` + 
+				` Link was added to clipboard.`);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+				await this.triggerBuild();
+			},
+			
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new NotePubliserSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -89,49 +54,86 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+
+	async uploadText(title: string, content: string) {
+		await fetch(this.settings.uploadFunctionUrl, {
+			method: 'POST',
+			body: JSON.stringify({
+				title,
+				content	
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+	}
+
+	async triggerBuild() {
+		await fetch(this.settings.buildWebHook);
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+
+class PublishedModal extends Modal {
+	url = "";
+	constructor(app: App, url: string = "") {
 		super(app);
+		this.url = url;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		let link = document
+			.createElement("a");
+		link.setAttribute("href", this.url)
+		link.setAttribute("target", "_blank")
+		link.innerText = this.url;
+		contentEl.appendChild(link)
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class NotePubliserSettingTab extends PluginSettingTab {
+	plugin: NotePublisher;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: NotePublisher) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Settings ' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('URL of upload function')
+			.setDesc('')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.uploadFunctionUrl)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.uploadFunctionUrl = value;
 					await this.plugin.saveSettings();
 				}));
+
+		new Setting(containerEl)
+			.setName('Build Web Hook')
+			.setDesc('The URL to GET to trigger a build')
+			.addText(text => text
+				.setPlaceholder('https://netlify.com/webhook/123')
+				.setValue(this.plugin.settings.buildWebHook)
+				.onChange(async (value) => {
+					this.plugin.settings.buildWebHook = value;
+					await this.plugin.saveSettings();
+				}));
+
 	}
 }
