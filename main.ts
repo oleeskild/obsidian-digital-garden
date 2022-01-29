e05b18418f0373dec68b2aea043fc6850cc3e6bd
Ole Eskild Steensen <ole.steensen@knowit.no>
2022-01-29 20:07:46 +0100

    Use github api instead of custom azure endpoint

--- main.ts
+++ main.ts
@@ -1,20 +1,21 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, getLinkpath } from 'obsidian';
 import { clipboard } from 'electron';
 import * as fs from "fs";
 import * as path from 'path';
+import { Octokit } from "@octokit/core";
 
 // Remember to rename these classes and interfaces!
 
 interface NotePublisherSettings {
-	uploadFunctionUrl: string;
-	buildWebHook: string;
-	attachmentFolder: string;
+	githubToken: string;
+	githubRepo: string;
+	githubUserName: string;
 }
 
 const DEFAULT_SETTINGS: NotePublisherSettings = {
-	uploadFunctionUrl: 'default',
-	buildWebHook: 'default',
-	attachmentFolder: 'attachments'
+	githubRepo: '',
+	githubToken: '',
+	githubUserName:''
 }
 
 export default class NotePublisher extends Plugin {
@@ -37,12 +38,9 @@ export default class NotePublisher extends Plugin {
 					await this.uploadText(view.file.name, text);
 					let fileTitle = view.file.name.replace(".md", "");
 					let urlFileTitle = encodeURI(fileTitle);
-					// new PublishedModal(this.app, `https://ole.dev/notes/${fileTitle}`).open();
-					clipboard.writeText(`https://ole.dev/notes/${urlFileTitle}`);
-					new Notice(`Successfully published to https://ole.dev/notes/${urlFileTitle}.` +
-						` Link was added to clipboard.`);
+					// clipboard.writeText(`https://ole.dev/notes/${urlFileTitle}`);
+					new Notice(`Successfully published note to your rock garden.`);
 
-					await this.triggerBuild();
 				} catch (e) {
 					console.error(e)
 					new Notice("Unable to publish note, something went wrong.")
@@ -70,22 +68,41 @@ export default class NotePublisher extends Plugin {
 
 
 	async uploadText(title: string, content: string) {
-		await fetch(this.settings.uploadFunctionUrl, {
-			method: 'POST',
-			body: JSON.stringify({
-				title,
-				content
-			}),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		});
-	}
+		const octokit = new Octokit({ auth: this.settings.githubToken });
 
-	async triggerBuild() {
-		await fetch(this.settings.buildWebHook, {
-			method: 'POST'
-		});
+		let base64Content = Buffer.from(content).toString('base64');
+		const path = `src/site/notes/${title}`
+
+		const payload = {
+			owner: this.settings.githubUserName,
+			repo: this.settings.githubRepo,
+			path,
+			message: `Add note ${title}`,
+			content: base64Content,
+			sha: ''
+		}; 
+		
+		let fileExists = true; 
+		try {
+			var response = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+				owner: this.settings.githubUserName,
+				repo: this.settings.githubRepo,
+				path
+			});
+		} catch (e) {
+			fileExists = false;
+			console.log("ERROR INCOMING");
+			console.log(e)
+		}
+
+
+
+		if (fileExists && response.status === 200 && response.data.type === "file")
+			payload.sha = response.data.sha;
+			payload.message = `Update note ${title}`;
+
+		await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', payload);
+
 	}
 
 	async getImage(filePath: string) {
@@ -100,7 +117,7 @@ export default class NotePublisher extends Plugin {
 		});
 	}
 
-	async createBase64Images(text: string):string {
+	async createBase64Images(text: string): string {
 		let imageText = text;
 		let imageRegex = /!\[\[(.*?)(\.(png|jpg|jpeg|gif))\]\]/g;
 		let imageMatches = text.match(imageRegex);
@@ -108,40 +125,16 @@ export default class NotePublisher extends Plugin {
 			for (let i = 0; i < imageMatches.length; i++) {
 				let imageMatch = imageMatches[i];
 				let imageName = imageMatch.substring(imageMatch.indexOf('[') + 2, imageMatch.indexOf(']'));
-				let imagePath = path.join(this.settings.attachmentFolder, imageName);
-				let absolutePath = path.join(this.app.vault.adapter.basePath, imagePath);
-				let image = await this.getImage(absolutePath);
-				let imageBase64 = image.toString('base64');
+				let imagePath = getLinkpath(imageName);
+				let linkedFile = this.app.metadataCache.getFirstLinkpathDest(imagePath, this.app.workspace.getActiveFile().path);
+				let image = await this.app.vault.readBinary(linkedFile);
+				let imageBase64 = arrayBufferToBase64(image)
 				let imageMarkdown = `![${imageName}](data:image/png;base64,${imageBase64})`;
 				imageText = imageText.replace(imageMatch, imageMarkdown);
 			}
 		}
 
 		return imageText;
-	}
-}
-
-
-class PublishedModal extends Modal {
-	url = "";
-	constructor(app: App, url: string = "") {
-		super(app);
-		this.url = url;
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		let link = document
-			.createElement("a");
-		link.setAttribute("href", this.url)
-		link.setAttribute("target", "_blank")
-		link.innerText = this.url;
-		contentEl.appendChild(link)
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
 
@@ -161,37 +154,48 @@ class NotePubliserSettingTab extends PluginSettingTab {
 		containerEl.createEl('h2', { text: 'Settings ' });
 
 		new Setting(containerEl)
-			.setName('URL of upload function')
-			.setDesc('')
+			.setName('Github repo')
+			.setDesc('The name of the github repo')
 			.addText(text => text
-				.setPlaceholder('')
-				.setValue(this.plugin.settings.uploadFunctionUrl)
+				.setPlaceholder('mydigitalgarden')
+				.setValue(this.plugin.settings.githubRepo)
 				.onChange(async (value) => {
-					this.plugin.settings.uploadFunctionUrl = value;
+					this.plugin.settings.githubRepo = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Github username')
+			.setDesc('Your GitHub username')
+			.addText(text => text
+				.setPlaceholder('myusername')
+				.setValue(this.plugin.settings.githubUserName)
+				.onChange(async (value) => {
+					this.plugin.settings.githubUserName= value;
 					await this.plugin.saveSettings();
 				}));
 
 		new Setting(containerEl)
-			.setName('Build Web Hook')
-			.setDesc('The URL to trigger a build (Using a POST request)')
+			.setName('Github token')
+			.setDesc('A github token')
 			.addText(text => text
-				.setPlaceholder('https://netlify.com/webhook/123')
-				.setValue(this.plugin.settings.buildWebHook)
+				.setPlaceholder('https://github.com/user/repo')
+				.setValue(this.plugin.settings.githubToken)
 				.onChange(async (value) => {
-					this.plugin.settings.buildWebHook = value;
+					this.plugin.settings.githubToken = value;
 					await this.plugin.saveSettings();
 				}));
-		new Setting(containerEl)
-			.setName('Attachments folder')
-			.setDesc('The folder where attachments are stored')
-			.addText(text => text
-				.setPlaceholder('attachments')
-				.setValue(this.plugin.settings.attachmentFolder)
-				.onChange(async (value) => {
-					this.plugin.settings.attachmentFolder= value;
-					await this.plugin.saveSettings();
-				}));
+
 
 
 	}
+}
+
+function arrayBufferToBase64(buffer: ArrayBuffer) {
+	var binary = "";
+	var bytes = new Uint8Array(buffer);
+	var len = bytes.byteLength;
+	for (var i = 0; i < len; i++) {
+		binary += String.fromCharCode(bytes[i]);
+	}
+	return window.btoa(binary);
 }
