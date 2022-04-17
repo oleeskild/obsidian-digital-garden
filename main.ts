117438d0edc2893d704d08c74488930657fd2029
Ole Eskild Steensen <ole.steensen@knowit.no>
2022-04-18 01:36:34 +0200

    Bump version

--- main.ts
+++ main.ts
@@ -30,7 +30,7 @@ export default class DigitalGarden extends Plugin {
 	publishModal: PublishModal;
 
 	async onload() {
-		this.appVersion = "2.9.0";
+		this.appVersion = "2.9.1";
 
 		console.log("Initializing DigitalGarden plugin v" + this.appVersion);
 		await this.loadSettings();
