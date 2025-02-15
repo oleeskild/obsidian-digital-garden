a7d5de83104fdf90fa893576e352407486bbed6c
Dazor Plasma <dazorplasma@proton.me>
2025-02-15 18:24:45 +0200

    sync

--- src/compiler/GardenPageCompiler.ts
+++ src/compiler/GardenPageCompiler.ts
@@ -447,6 +447,7 @@ export class GardenPageCompiler {
 											linkedFile.path,
 											this.rewriteRules,
 										),
+										this.settings.slugifyEnabled,
 								  )}`;
 							embedded_link = `<a class="markdown-embed-link" href="${gardenPath}${sectionID}" aria-label="Open link"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg></a>`;
 						}
