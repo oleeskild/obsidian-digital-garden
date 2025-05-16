defdabe5d9f58154e879aff2dce2acc75b6842fc
Emile Bangma <github@emilebangma.com>
2025-05-16 09:06:10 +0200

    Hotfix: tranclusion links with subfolder (#42)

--- src/compiler/SyncerPageCompiler.ts
+++ src/compiler/SyncerPageCompiler.ts
@@ -478,9 +478,19 @@ export class SyncerPageCompiler {
 										),
 								  );
 
-							const quartzPath = quartzPathFull.endsWith("/")
+							let quartzPath = quartzPathFull.endsWith("/")
 								? quartzPathFull.slice(0, -1)
 								: quartzPathFull;
+
+							if (
+								this.settings.vaultPath !== "/" &&
+								this.settings.vaultPath !== ""
+							) {
+								quartzPath = quartzPath.replace(
+									this.settings.vaultPath,
+									"",
+								);
+							}
 
 							embedded_link = `<a class="markdown-embed-link" href="${quartzPath}${sectionID}" aria-label="Open link"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg-icon lucide-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path></svg></a>`;
 						}
