910946e051f5f8e80a3f587848b8615e57617ead
KULA-Citizen-Science <christian_schmidt@mailbox.org>
2025-10-22 15:30:52 -0700

    Update files

---
+++ src/site/notes/notes.11tydata.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const settings = require("../../helpers/constants");
+
+const allSettings = settings.ALL_NOTE_SETTINGS;
+
+module.exports = {
+  eleventyComputed: {
+    layout: (data) => {
+      if (data.tags.indexOf("gardenEntry") != -1) {
+        return "layouts/index.njk";
+      }
+      return "layouts/note.njk";
+    },
+    permalink: (data) => {
+      if (data.tags.indexOf("gardenEntry") != -1) {
+        return "/";
+      }
+      return data.permalink || undefined;
+    },
+    settings: (data) => {
+      const noteSettings = {};
+      allSettings.forEach((setting) => {
+        let noteSetting = data[setting];
+        let globalSetting = process.env[setting];
+
+        let settingValue =
+          noteSetting || (globalSetting === "true" && noteSetting !== false);
+        noteSettings[setting] = settingValue;
+      });
+      return noteSettings;
+    },
+  },
+};
