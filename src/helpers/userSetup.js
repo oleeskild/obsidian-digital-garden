777d2cc2d6aad8335b19076696d4dfde4f37211a
KULA-Citizen-Science <christian_schmidt@mailbox.org>
2025-10-22 15:31:29 -0700

    Update files

---
+++ src/helpers/userSetup.js
@@ -0,0 +1,10 @@
+function userMarkdownSetup(md) {
+  // The md parameter stands for the markdown-it instance used throughout the site generator.
+  // Feel free to add any plugin you want here instead of /.eleventy.js
+}
+function userEleventySetup(eleventyConfig) {
+  // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
+  // Feel free to add any plugin you want here instead of /.eleventy.js
+}
+exports.userMarkdownSetup = userMarkdownSetup;
+exports.userEleventySetup = userEleventySetup;
