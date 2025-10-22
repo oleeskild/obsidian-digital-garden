3243a06cb0148a6db81dfdb769245d987dead508
KULA-Citizen-Science <christian_schmidt@mailbox.org>
2025-10-22 15:31:18 -0700

    Update files

---
+++ src/site/_data/eleventyComputed.js
@@ -0,0 +1,9 @@
+const { getGraph } = require("../../helpers/linkUtils");
+const { getFileTree } = require("../../helpers/filetreeUtils");
+const { userComputed } = require("../../helpers/userUtils");
+
+module.exports = {
+  graph: (data) => getGraph(data),
+  filetree: (data) => getFileTree(data),
+  userComputed: (data) => userComputed(data)
+};
