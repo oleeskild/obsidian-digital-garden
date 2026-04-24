e4654686c6fefd5981407b5c5311b1d8b0a6c14d
asoleto77 <98430866+asoleto77@users.noreply.github.com>
2026-04-24 00:22:04 -0400

    Update files

---
+++ src/site/notes/notes.11tydata.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const settings = require("../../helpers/constants");
+
+const allSettings = settings.ALL_NOTE_SETTINGS;
+
+module.exports = {
+  eleventyComputed: {
+    layout: (data) => {
+      if (data.tags.indexOf("gardenEntry") != -1) {
+        return "layouts/index.njk";
+      }
+      return "layouts/note.njk";
+    },
+    permalink: (data) => {
+      if (data.tags.indexOf("gardenEntry") != -1) {
+        return "/";
+      }
+      return data.permalink || undefined;
+    },
+    basesNotes: (data) => {
+      if (!data.collections || !data.collections.note) return [];
+      return data.collections.note.map((item) => ({
+        path: item.filePathStem.replace("/notes/", ""),
+        url: item.url,
+        metadata: item.data,
+        fileSlug: item.fileSlug,
+      }));
+    },
+    settings: (data) => {
+      const noteSettings = {};
+      allSettings.forEach((setting) => {
+        let noteSetting = data[setting];
+        let globalSetting = process.env[setting];
+
+        let settingValue =
+          noteSetting || (globalSetting === "true" && noteSetting !== false);
+        noteSettings[setting] = settingValue;
+      });
+      return noteSettings;
+    },
+  },
+};
