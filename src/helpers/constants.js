e6d529c78ed7ec083b30c0848c1aabe783971b23
admonth1224 <moon@planall.co.kr>
2026-06-03 16:16:16 +0900

    Update files

---
+++ src/helpers/constants.js
@@ -0,0 +1,12 @@
+exports.ALL_NOTE_SETTINGS= [
+    "dgHomeLink",
+    "dgPassFrontmatter",
+    "dgShowBacklinks",
+    "dgShowLocalGraph",
+    "dgShowInlineTitle",
+    "dgShowFileTree",
+    "dgEnableSearch",
+    "dgShowToc",
+    "dgLinkPreview",
+    "dgShowTags"
+];
